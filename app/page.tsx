c8ab76db81abb98c80c986605fe3b62136e6ce55
Alexa VanSchaardenburg <alexa.vsdb@gmail.com>
2024-11-17 17:53:43 -0700

    Wrapped up final stylinng changes to match the designs

--- app/page.tsx
+++ app/page.tsx
@@ -16,7 +16,7 @@ import { CMS_NAME, CMS_URL } from "@/lib/constants";
 function Intro() {
   return (
     <section className="flex-col md:flex-row flex items-center md:justify-center mt-16 mb-16 md:mb-12">
-      <img src={pocketChange.src} style={{ width: '40%', height: 'auto' }} />
+      <img src={pocketChange.src} style={{ width: '50%', height: 'auto' }} />
     </section>
   );
 }
@@ -41,21 +41,17 @@ function HeroPost({
       <div className="mb-8 md:mb-16">
         <CoverImage title={title} slug={slug} url={coverImage.url} />
       </div>
-      <div className="md:grid md:gap-x-16 lg:gap-x-8 mb-20 md:mb-28 border-2 border-green-500">
-        {/* <div> */}
+      <div className="md:grid md:gap-x-16 lg:gap-x-8 mb-20 md:mb-28">
           <h3 className="mb-4 text-4xl lg:text-6xl leading-tight">
             <Link href={`/posts/${slug}`} className="hover:underline font-bold">
               {title}
             </Link>
           </h3>
-          <div className="mb-4 md:mb-0 text-lg">
+          <div className="mb-4 md:mb-0 text-lg pb-[20px]">
             <Date dateString={date} />
           </div>
-        {/* </div> */}
-        {/* <div> */}
           <p className="text-lg leading-relaxed mb-4">{excerpt}</p>
           {author && <Avatar name={author.name} picture={author.picture} />}
-        {/* </div> */}
       </div>
     </section>
   );
